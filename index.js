83ea3dab124c6915de38f508f9c28665266faf4a
mikhail <mikhail.sergienko@gmail.com>
2023-10-21 17:45:12 +0300

    (style): merge from `main` + action

--- index.js
+++ index.js
@@ -1,49 +1,70 @@
 import process from 'node:process';
 import {
-	flattenJSON,
-	readJSON,
-	removeKeys,
-	removeSameKeyValues,
-	writeJSONandTSV,
+  flattenJSON,
+  readJSON,
+  compose,
+  removeCommonKeys,
+  writeJSONandTSV,
 } from './js/util.js';
 
-const excludedKeys = [
-	'plan.id',
-	'plan.name',
-	'development_mode',
-];
+/** Constants for keys that need specific treatments */
+const IGNORED_KEYS = ['plan.id', 'plan.name', 'development_mode'];
+const DATE_KEYS = ['created_on', 'updated_on', 'activated_on', 'modified_on'];
+const DATE_FORMAT_PROPS = ['da', { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' }];
 
-const keptKeys = [];
-
-const processJSON = inputJSON => {
-	const processItem = o => {
-		const flat = flattenJSON(o);
-		const result = removeKeys(flat, excludedKeys);
-		return result;
-	};
-
-	const processArray = (array = []) => {
-		let result = array.map(o => processItem(o));
-		result = removeSameKeyValues(result, keptKeys);
-		return result;
-	};
-
-	const result = processArray(inputJSON);
-	return result;
+/**
+ * Excludes given keys from an object.
+ * @param {Array} keysToExclude - Keys to exclude from the object.
+ * @return {Function} Function that excludes keys from an object.
+ */
+const excludeKeysFromObject = keysToExclude => object => {
+  for (const key of keysToExclude) {
+    delete object[key];
+  }
+  return object;
 };
 
+/**
+ * Formats date strings in an object.
+ * @param {Array} keys - Keys to search and format.
+ * @param {Function} formatFn - Function that formats the date.
+ * @return {Function} Function that formats date strings in an object.
+ */
+const formatDates = (keys, formatFn) => object => {
+  for (const key of keys) {
+    if (Object.hasOwn(object, key)) {
+      object[key] = formatFn(object[key]);
+    }
+  }
+  return object;
+};
+
+/**
+ * Processes input data with several operations.
+ * @param {Array} data - Input data.
+ * @return {Array} Processed data.
+ */
+const processData = data => {
+  const operations = compose(
+    flattenJSON,
+    formatDates(DATE_KEYS, date => new Date(date).toLocaleDateString(...DATE_FORMAT_PROPS)),
+    excludeKeysFromObject(IGNORED_KEYS),
+  );
+
+  return removeCommonKeys(data.map(item => operations(item)));
+};
+
+/**
+ * Main execution function.
+ */
 const main = async () => {
-	const file = process.argv[2];
+  const inputFile = process.argv[2];
+  const rawData = await readJSON(inputFile);
+  await writeJSONandTSV(inputFile, rawData);
 
-	const json = await readJSON(file);
-	await writeJSONandTSV(file, json);
-
-	const outputJSON = processJSON(json);
-	const outputFile = file.replace('raw/', '');
-
-	console.log({outputFile});
-
-	await writeJSONandTSV(outputFile, outputJSON);
+  const processedData = processData(rawData);
+  const outputFile = inputFile.replace('raw/', '');
+  await writeJSONandTSV(outputFile, processedData);
 };
 
-await main();
+main();
--- js/util.js
+++ js/util.js
@@ -1,118 +1,164 @@
 import fs from 'node:fs/promises';
 
+/**
+ * Composes multiple functions into one.
+ * @param {...Function} fns - Functions to compose.
+ * @return {Function} Composed function.
+ */
+export const compose = (...fns) => x => fns.reduce((v, f) => f(v), x);
+
+/**
+ * Reads a JSON file and returns its content.
+ * @param {string} file - Path to the file.
+ * @return {Object} Parsed JSON content.
+ * @throws Will throw an error if reading or parsing fails.
+ */
 export const readJSON = async file => {
-	try {
-		const data = await fs.readFile(file, 'utf8');
-		return JSON.parse(data);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const data = await fs.readFile(file, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Error reading JSON from ${file}:`, error);
+    throw error;
+  }
 };
 
+/**
+ * Writes data to a JSON file.
+ * @param {string} file - Path to the file.
+ * @param {Object} data - Data to write.
+ * @param {Function} replacer - Replacer function.
+ * @param {number} space - Number of spaces to indent.
+ * @throws Will throw an error if writing fails.
+ */
 export const writeJSON = async (file, data, replacer = null, space = 2) => {
-	try {
-		const json = JSON.stringify(data, replacer, space);
-		await fs.writeFile(file, json);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const json = JSON.stringify(data, replacer, space);
+    await fs.writeFile(file, json);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-const arraytoCSV = (array, delim = '\t') => {
-	if (array.length === 0) {
-		throw new Error('Array is empty. Cannot write CSV file.');
-	}
-
-	const keysSet = new Set(array.flatMap(o => Object.keys(o)));
-	const header = [...keysSet].join(delim);
-
-	const rows = [];
-
-	for (const object of array) {
-		const row = [];
-		for (const key of keysSet) {
-			const value = object[key];
-
-			if (typeof value === 'string' && value.includes(delim)) {
-				row.push(`"${value}"`);
-				continue;
-			}
-
-			row.push(value);
-		}
-
-		rows.push(row.join(delim));
-	}
-
-	const csv = [header, ...rows].join('\n');
-	return csv;
+/**
+ * Escapes a value for CSV.
+ * @param {string} value - Value to escape.
+ * @return {string} Escaped value.
+ */
+export const escapeCSV = value => {
+  if (typeof value !== 'string') {
+    return value;
+  }
+  return `"${value.replace(/"/g, '""')}"`;
 };
+
+/**
+ * Converts an array of objects to a CSV string.
+ * @param {Array} array - Array of objects.
+ * @param {string} delim - Delimiter to use.
+ * @return {string} CSV string.
+ * @throws Will throw an error if the array is empty.
+ */
+
+export const arraytoCSV = (array, delim = '\t') => {
+  if (array.length === 0) {
+    throw new Error('Array is empty. Cannot write CSV file.');
+  }
+
+  const keys = [...new Set(array.flatMap(o => Object.keys(o)))];
+
+  const header = keys.map(k => escapeCSV(k)).join(delim);
+  const rows = array.map(o => keys.map(key => escapeCSV(o[key])).join(delim),
+  );
+
+  return [header, ...rows].join('\n');
+};
+
+/**
+ * Writes data to a CSV file.
+ * @param {string} file - Path to the file.
+ * @param {Array} array - Array of objects to write.
+ * @param {string} delim - Delimiter to use.
+ * @throws Will throw an error if writing fails.
+ */
 
 export const writeCSV = async (file, array, delim) => {
-	try {
-		const csv = arraytoCSV(array, delim);
-		await fs.writeFile(file, csv);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    const csv = arraytoCSV(array, delim);
+    await fs.writeFile(file, csv);
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+/**
+ * Writes data to a JSON and a TSV file.
+ * @param {string} file - Path to the file.
+ * @param {Object} data - Data to write.
+ * @throws Will throw an error if writing fails.
+ */
 
 export const writeJSONandTSV = async (file, data) => Promise.all([
-	await writeJSON(file, data, null, 2),
-	await writeCSV(file.replace(/json$/, 'tsv'), data, '\t'),
+  await writeJSON(file, data, null, 2),
+  await writeCSV(file.replace(/json$/, 'tsv'), data, '\t'),
 ]);
 
+/**
+ * Flattens a JSON object.
+ * @param {Object} json - JSON object to flatten.
+ * @return {Object} Flattened JSON object.
+ *
+ * Example:
+ *
+ * Input:   { a: { b: { c: 1 } } }
+ * Output:  { a.b.c: 1 }
+ */
+
 export const flattenJSON = json => {
-	const result = {};
-	for (const [key, value] of Object.entries(json)) {
-		if (value && !Array.isArray(value) && typeof value === 'object') {
-			for (const [subKey, subValue] of Object.entries(value)) {
-				result[`${key}.${subKey}`] = subValue;
-			}
-		} else {
-			result[key] = value;
-		}
-	}
-
-	return result;
+  const result = {};
+  for (const [key, value] of Object.entries(json)) {
+    if (value && !Array.isArray(value) && typeof value === 'object') {
+      for (const [subKey, subValue] of Object.entries(value)) {
+        result[`${key}.${subKey}`] = subValue;
+      }
+    } else {
+      result[key] = value;
+    }
+  }
+  return result;
 };
 
-export const removeSameKeyValues = (array, keep = []) => {
-	const keysToCheck = Array.from(
-		new Set(array.flatMap(object => Object.keys(object))),
-	);
+/**
+ * Removes common keys from an array of objects.
+ * @param {Array} items - Array of objects.
+ * @return {Array} Array of objects without common keys.
+ *
+ * Example:
+ *
+ * Input:    [{ a: 1, b: 2 },{ a: 1, c: 3 }]
+ * Output:   [{ b: 2 },{ c: 3 }]
+ */
 
-	const keysToRemove = keysToCheck.filter(key => {
-		if (keep.includes(key)) {
-			return false;
-		}
+export const removeCommonKeys = items => {
+  if (items.length === 0) {
+    return [];
+  }
 
-		let result = true;
+  const firstObject = items[0];
 
-		if (typeof array[0][key] === 'object') {
-			result = array.every(o =>
-				JSON.stringify(o[key]) === JSON.stringify(array[0][key]),
-			);
-		} else {
-			result = array.every(o => o[key] === array[0][key]);
-		}
+  const keysToRemove = new Set(Object.keys(firstObject).filter(key =>
+    items.every(o => o[key] === firstObject[key]),
+  ));
 
-		return result;
-	});
-
-	for (const object of array) {
-		for (const key of keysToRemove) {
-			delete object[key];
-		}
-	}
-
-	return array;
-};
-
-export const removeKeys = (o, keys) => {
-	for (const key of keys) {
-		delete o[key];
-	}
-
-	return o;
+  return items.map(o => {
+    const newObject = {};
+    for (const key of Object.keys(o)) {
+      if (keysToRemove.has(key)) {
+        continue;
+      } else {
+        newObject[key] = o[key];
+      }
+    }
+    return newObject;
+  });
 };
